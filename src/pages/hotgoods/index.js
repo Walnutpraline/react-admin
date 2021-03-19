dd18f0ed1dcf84df698df96358d9bf91e9e7ef63
253940longxue <shixiaolong@yohitrip.com>
2021-03-19 18:01:55 +0800

    2021.3.19

--- src/pages/hotgoods/index.js
+++ src/pages/hotgoods/index.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { useState, useEffect } from "react";
 
-class HotGoods extends React.Component {
-    render() {
-        return (
-            <div>
-                <h3>HotGoods</h3>
-            </div>
-        )
+// import { useState, useEffect } from 'react';
+// class HotGoods extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             count: { a: 1 }
+//         }
+//     }
+//     add = () => {
+//         this.setState({ count: { a: this.state.count.a + 1 } })
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h3>HotGoods</h3>
+//                 <p>{this.state.count.a}</p>
+//                 <button onClick={this.add}>add</button>
+//             </div>
+//         )
+//     }
+// }
+function HotGoods() {
+    const [count, setCount] = useState({ a: 0 });
+    function add() {
+        console.log(count, "count")
+        setCount({ a: count.a + 1 })
     }
+    return (
+        <div>
+            Count: {count.a}
+            <button onClick={add}>+</button>
+        </div>
+    );
 }
 export default HotGoods;
